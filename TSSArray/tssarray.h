cff44685cf630346d149b4d86eb07a4271cbe59f
cammyteee <wirelessatari@gmail.com>
2017-10-31 15:17:12 -0800

    style fixes

--- TSSArray/tssarray.h
+++ TSSArray/tssarray.h
@@ -52,100 +52,87 @@ public:
 	{}
 
 	//COPY CTOR
-	TSSArray(const TSSArray & rhs):_capacity(rhs._capacity),_size(rhs._size), _data(rhs._data)
-	{
+	TSSArray(const TSSArray & rhs):_capacity(rhs._capacity),_size(rhs._size), _data(rhs._data) {
 		std::copy(rhs.begin(), rhs.end(), begin());
 	}
 
 	//MOVE CTOR
-	TSSArray(const TSSArray && rhs):_capacity(rhs._capacity),_size(rhs._size), _data(rhs._data)
-	{
+	TSSArray(const TSSArray && rhs) noexcept :_capacity(rhs._capacity),_size(rhs._size), _data(rhs._data) {
 		rhs._capacity = 0;
 		rhs._size = 0;
 		rhs._data = nullptr;
 	}
+
 	//DCTOR NO THROW
-	~TSSArray(){
+	~TSSArray() {
 		delete[] _data;
 	}
+
 	//COPY ASSIGNMENT
 	TSSArray & operator=(const TSSArray & rhs){
 		return *this;
 	} 
+
 	//MOVE ASSIGNMENT NO THROW
-	TSSArray & operator=(TSSArray && rhs) noexcept
-	{
+	TSSArray & operator=(TSSArray && rhs) noexcept {
 		return *this;
 	}
 
 //MEMBER OPERATOR FUNCTIONS
 
 	//OPERATOR[]
-	value_type & operator[](size_type size)
-	{
+	value_type & operator[](size_type size) {
 		return _data[size];
 	}
 	//CONST OPERATOR[]
-	value_type & operator[](size_type size) const
-	{
+	value_type & operator[](size_type size) const {
 		return _data[size];
 	}
 
 //GENERAL OPERATOR FUNCTIONS
 
-	size_type size() const
-	{
+	size_type size() const {
 		return _size;
 	}
 
-	bool empty() const
-	{
+	bool empty() const {
 		return size() == size_type(0);
 		//return true;
 	}
 
-	void resize(size_type size)
-	{
+	void resize(size_type size) {
 		//TODO DUMMY
 	}
 
-	iterator insert(iterator i, const value_type & v)
-	{
+	iterator insert(iterator i, const value_type & v) {
 		return i;
 	}
 
-	iterator erase(iterator i)
-	{
+	iterator erase(iterator i) {
 		return i;
 	}
 
-	iterator begin()
-	{
+	iterator begin() {
 		return _data;
 	}
 
-	iterator begin() const
-	{
+	iterator begin() const {
 		return _data;
 	}
 
-	iterator end()
-	{
+	iterator end() {
 		return begin() + size();
 	}
 
-	iterator end() const
-	{
+	iterator end() const {
 		return begin() + size();
 	}
 
-	void push_back(const value_type & v)
-	{
+	void push_back(const value_type & v) {
 
 	}
 
-	void pop_back()
-	{
+	void pop_back() {
 		
 	}
 
