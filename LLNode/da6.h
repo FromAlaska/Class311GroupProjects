3c3835063fafc9e8eb71792363ae4a8f7abaeb2d
Samuel Grenon <sgrenon@alaska.edu>
2017-11-08 21:27:48 -0900

    COMPILES BUT SEGFAULT

--- LLNode/da6.h
+++ LLNode/da6.h
@@ -1,28 +1,80 @@
-// da6.h
-// Jim Samson
-// 08 November 2017
-//
-// For CS311 Fall 2017
-// 
-
+#ifndef da6_h
+#define da6_h
 #include "llnode2.h"
-
-// reverseList(shared_ptr<LLNode2<ValType>>);
-// This function is given a shared_pt to a Linked List-an empty shared_ptr if the
-// List is empty. It alters the list so that the values appear in reverse order.
-// A pointer  to the new list is reterned via the reference parameter.
-template<typname ValType>
-void reverseList(shared_ptr<LLNode2<Valtype>> &head) noexcept
-{
-
-}
+#include <cstddef>
+#include <iostream>
+#include <functional>
+using std::size_t;
 
 template<typename ValType>
+void reverseList(shared_ptr<LLNode2<ValType> > & head) {
+	//make_shared<ValType> next;
+	//make_shared<ValType> prev;
+	//auto current = head;
+
+	//while (current != null) {
+	//	next = current->_next;
+	//	current->next = prev;
+	//	prev = current;
+	//	current = next;
+
+
+	//}
+	//head = prev
+}
+
+template<typename KeyType, typename ValType>
 class LLMap {
- LLMap()
+private:
+	using KVTYPE = std::pair<KeyType, ValType>;
+	using _dummy = shared_ptr<LLNode2<KVTYPE>>;
+
 public:
 
+	LLMap()  //_dummy(nullptr)
+	{}
 
-private:
+	int size() {
+		return 1; // TODO
+	}
+	int size() const {
+		return 1; // TODO
+	}
+
+	bool empty() {
+		return true; // TODO
+	}
+	bool empty() const {
+		return true;
+	}
+	
+	ValType* find(const KeyType & key) {
+		return nullptr;
+		// TODO
+	}
+
+	const ValType* find(const KeyType & list) const {
+		return nullptr;
+	}
+
+	void insert(const KeyType & key, const ValType & val) {
+
+	}
+
+	void erase(const KeyType & key) {
+
+	}
+
+	void traverse(std::function<void(KeyType, ValType)> function) {
+
+	}
+	
+public:
+	// BIG FIVE
+	~LLMap() = default;
+	LLMap(const LLMap&) = delete;
+	LLMap(LLMap&&) = delete;
+	LLMap & operator=(const LLMap) = delete;
+	LLMap & operator=(LLMap &&) = delete;
 };
-
+#endif
